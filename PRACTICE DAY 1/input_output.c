5f59ed35c59c3dd11e899ab3ab91fb5490aa5658
MASH <128233548+Mash6071@users.noreply.github.com>
2023-04-04 01:11:08 +0600

    Update input_output.c

--- PRACTICE DAY 1/input_output.c
+++ PRACTICE DAY 1/input_output.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    printf("Recently I heard that you’ve achieved 95% marks in your exam.\n");
+    printf("Recently I heard that you’ve achieved 95%% marks in your exam.\n");
     printf("This is brilliant!\n");
     printf("I wish you’ll shine in your life!\tGood luck with all the barriers(/\\) in your life.");
 
